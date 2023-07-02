99397e124ecd907d852493a76e32bfaad5ecfe96
Ritwik <ritwikbhowmik659@gmail.com>
2023-07-02 19:52:15 +0530

    bubble sorting algoritm

---
+++ sorting-alog/bubble_sort.cpp
@@ -0,0 +1,32 @@
+//bubble sort
+#include<bits/stdc++.h>
+using namespace std;
+void bubble_sort(int arr[],int n){
+
+    for(int i=0;i<n-1;i++){
+        for(int j=0;j<n-i-1;j++){
+            if(arr[j]>arr[j+1]){
+
+                int tem=arr[j];
+                arr[j]=arr[j+1];
+                arr[j+1]=tem;
+            }
+        }
+    }
+
+}
+void printarray(int arr[],int n){
+
+    for(int i=0;i<n;i++){
+        cout<<arr[i]<<" ";
+    }
+}
+int main(){
+
+    int arr[]={12,1,5,3,1,10};
+    int n=6;
+    printarray(arr,n);
+    cout<<endl;
+    bubble_sort(arr,n);
+    printarray(arr,n);
+}
