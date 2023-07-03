feb4ffcdc163705c0fa11cd3b3cb3a81d566d2be
Ritwik <ritwikbhowmik659@gmail.com>
2023-07-03 11:41:53 +0530

    this is a modified version

--- Arrays/second_max_minelement.cpp
+++ Arrays/second_max_minelement.cpp
@@ -13,10 +13,27 @@ int second_maxelement(int arr[],int n){
     }
     return second_max;
 }
+int second_min(int arr[],int n){
+
+    int minele=arr[0];
+    for(int i=0;i<n;i++){
+        minele=min(minele,arr[i]);
+    }
+    int second_min=INT_MAX;
+    for(int i=0;i<n;i++){
+        if(arr[i]<second_min && arr[i]!=minele){
+            second_min=arr[i];
+        }
+    }
+    return second_min;
+}
 int main()
 {
     int arr[]={12,1,5,6,7,9};
     int n=sizeof(arr)/sizeof(int);
     int second_max=second_maxelement(arr,n);
     cout<<second_max;
+    int second_minimum=second_min(arr,n);
+    cout<<endl;
+    cout<<second_minimum;
 }
